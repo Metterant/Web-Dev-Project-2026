45203c72dcba873b4ebce83e67ae3b290b3b407f
Metterant <metterantdev@gmail.com>
2026-05-13 03:04:52 +0700

    add .gitignore, refactor App component, and implement proxy middleware

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = 5000;
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 // Define a route for GET requests to the root URL
 app.get("/api", (req, res) => {
-    res.send("Hello World from Express!");
+    res.json({ tests: ["one", "two", "three"] })
 });
 
 // Start the server
