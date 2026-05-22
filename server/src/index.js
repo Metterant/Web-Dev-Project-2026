01c49d592d6454024a96a74609564ed19f4f7cbd
Metterant <metterantdev@gmail.com>
2026-05-23 03:32:18 +0700

    Add instructor routes

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 
 const studentRoutes = require('#routes/studentRoutes');
+const InstructorRoutes = require('#routes/instructorRoutes');
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -13,13 +14,16 @@ app.use((req, res, next) => {
 
 // Define a route for GET requests to the root URL
 app.get("/api", (req, res) => {
-    res.send('Request to /api/students/ to get students')
+    res.send('Request to /api/students/ to get students or /api/instructors to get instructors')
 });
 
 // Define a route for requests to /students
-app.use("/api/students", studentRoutes)
+app.use("/api/students", studentRoutes);
+
+// Define a route for requests to /students
+app.use("/api/instructors", InstructorRoutes);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
 });
