b811e2d6b0b2f99d0fec07faccb946f9738f0317
Metterant <metterantdev@gmail.com>
2026-05-24 01:17:20 +0700

    Add course routes for CRUD operations

---
+++ server/src/routes/courseRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/courseController');
+
+// GET requests hit /search
+router.get('/search', controller.searchCourses);
+
+// GET requests hit /delete
+router.get('/delete/:id', controller.deleteCourseRecord);
+
+// GET requests hit /:id
+router.get('/:id', controller.getCourseRecord);
+
+// GET requests hit / (root)
+router.get('/', controller.getAllCourses);
+
+// POST requests hit / (root)
+router.post('/', controller.createCourse);
+
+// PUT requests hit /:id
+router.put('/:id', controller.updateCourse);
+
+module.exports = router;
