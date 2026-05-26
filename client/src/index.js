0782d3752ad9d56b3f1956544adc61ebc59ce98b
Metterant <metterantdev@gmail.com>
2026-05-26 20:46:36 +0700

    Refactor routing setup and update CSS styles for improved design consistency

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
+
+import App from './App';
+import Login from './pages/Auth/Login';
+import ResetPassword from './pages/Auth/ResetPassword';
+
+const router = createBrowserRouter([
+  { path: '/', element: <App />},
+  { path: '/login', element: <Login />},
+  { path: '/reset_password', element: <ResetPassword />}
+  // { path: '/dashboard'}
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
