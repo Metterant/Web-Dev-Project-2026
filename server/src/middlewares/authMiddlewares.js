f098ee965544d43dca3e279b9a6de45561c11902
Metterant <metterantdev@gmail.com>
2026-05-26 21:42:48 +0700

    Add authentication middleware for token verification and role-based access control

---
+++ server/src/middlewares/authMiddlewares.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+
+  if (!token)
+    return res.status(401).json({ message: 'No token provided' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Invalid or expired token' });
+  }
+};
+
+const allowedRoles = (roles) => {
+  return (req, res, next) => {
+    console.log('User from token:', req.user);
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Access denied: insufficient permissions' });
+    } else {
+      next();
+    }
+  };
+}
+module.exports = { authMiddleware, allowedRoles };
